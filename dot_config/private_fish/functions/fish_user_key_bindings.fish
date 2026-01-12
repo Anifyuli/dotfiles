100096bae34033be982601c2758271fdb29e6117
Anifyuli <anifyuliansyah@gmail.com>
2026-01-12 22:54:03 +0700

    init: chezmoi dotfiles

---
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+# Override fish keybindings
+function fish_user_key_bindings
+	bind --erase --preset \es
+	bind \e\e 'for cmd in sudo doas please; if command -q $cmd; fish_commandline_prepend $cmd; break; end; end'
+end
