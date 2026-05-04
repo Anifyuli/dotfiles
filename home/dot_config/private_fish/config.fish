9e8193106d21032375d262167a8f7c988f7ae545
Anifyuli <anifyuli007@outlook.co.id>
2026-05-05 06:05:35 +0700

    fix: fish so slower than before

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    abbr --add warpstat 'curl https://www.cloudflare.com/cdn-cgi/trace/'
+
 end
 
 # Ensure $HOME/.local/bin is in PATH
@@ -21,14 +21,4 @@ for dir in \
     end
 end
 
-# Add NPM bin to $PATH
-if not contains -- $HOME/.npm/bin $PATH
-    set -gx PATH $HOME/.npm/bin $PATH
-end
-
-# $PNPM_HOME
-set -gx PNPM_HOME $HOME/.local/share/pnpm
-if not contains -- $PNPM_HOME $PATH
-    set -gx PATH $PNPM_HOME $PATH
-end
-
+mise activate fish | source
