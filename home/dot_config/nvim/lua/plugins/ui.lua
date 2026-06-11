8af0b4db47ead81bf0d00e1626c1fe6e4f50199e
Anifyuli <anifyuli007@outlook.co.id>
2026-06-11 15:49:28 +0700

    Replace cokeline with bufferline.nvim (sidebar offset for neo-tree)

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -111,165 +111,39 @@ return {
 
 
   {
-    -- Cokeline, buffer list
-    "willothy/nvim-cokeline",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- Required for v0.4.0+
-      "nvim-tree/nvim-web-devicons", -- If you want devicons
-      "stevearc/resession.nvim", -- Optional, for persistent history
-    },
-    event = "BufEnter",
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "UIEnter",
     config = function()
-      -- This config using https://github.com/noib3/ configs
-      local get_hex = require("cokeline.hlgroups").get_hl_attr
-      local mappings = require("cokeline/mappings")
-      local comments_fg = get_hex("Comment", "fg")
-      local errors_fg = get_hex("DiagnosticError", "fg")
-      local warnings_fg = get_hex("DiagnosticWarn", "fg")
-      local red = vim.g.terminal_color_1
-      local green = vim.g.terminal_color_2
-      local yellow = vim.g.terminal_color_3
-      local text_bold = function(buffer)
-        return ((buffer.is_focused and buffer.diagnostics.errors ~= 0) and true)
-          or (buffer.is_focused and true)
-          or (buffer.diagnostics.errors ~= 0 and false)
-          or false
-      end
-      local components = {
-        space = {
-          text = " ",
-          truncation = { priority = 1 },
-        },
-        two_spaces = {
-          text = "  ",
-          truncation = { priority = 1 },
-        },
-        separator = {
-          text = function(buffer)
-            return buffer.index ~= 1 and " ▏" or ""
-          end,
-          truncation = { priority = 1 },
-        },
-        devicon = {
-          text = function(buffer)
-            return (mappings.is_picking_focus() or mappings.is_picking_close()) and buffer.pick_letter .. " "
-              or buffer.devicon.icon
-          end,
-          fg = function(buffer)
-            return (mappings.is_picking_focus() and yellow)
-              or (mappings.is_picking_close() and red)
-              or buffer.devicon.color
-          end,
-          bold = function(_)
-            return (mappings.is_picking_focus() or mappings.is_picking_close()) and "true" or false
-          end,
-          italic = function(_)
-            return (mappings.is_picking_focus() or mappings.is_picking_close()) and "true" or false
-          end,
-          truncation = { priority = 1 },
-        },
-        index = {
-          text = function(buffer)
-            return buffer.index .. ":"
-          end,
-          truncation = { priority = 1 },
-          bold = text_bold,
-          underline = function(buffer)
-            return buffer.is_focused and true or false
-          end,
-        },
-        unique_prefix = {
-          text = function(buffer)
-            return buffer.unique_prefix
-          end,
-          fg = comments_fg,
-          italic = true,
-          truncation = {
-            priority = 3,
-            direction = "left",
+      require("bufferline").setup({
+        options = {
+          mode = "buffers",
+          offsets = {
+            {
+              filetype = "neo-tree",
+              text = "󰙅 Files",
+              text_align = "left",
+              highlight = "Directory",
+              separator = false,
+            },
           },
-        },
-        filename = {
-          text = function(buffer)
-            return buffer.filename
+          buffer_close_icon = "",
+          modified_icon = "●",
+          close_icon = "",
+          show_buffer_close_icons = true,
+          show_close_icon = false,
+          separator_style = "thin",
+          always_show_bufferline = true,
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = ""
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and "  " or (e == "warning" and "  " or "  ")
+              s = s .. n .. sym
+            end
+            return s
           end,
-          truncation = {
-            priority = 2,
-            direction = "left",
-          },
-          bold = text_bold,
-          underline = function(buffer)
-            return ((buffer.is_focused and buffer.diagnostics.errors ~= 0) and true)
-              or (buffer.is_focused and false)
-              or (buffer.diagnostics.errors ~= 0 and true)
-              or false
-          end,
-        },
-        diagnostics = {
-          text = function(buffer)
-            return (buffer.diagnostics.errors ~= 0 and "  " .. buffer.diagnostics.errors)
-              or (buffer.diagnostics.warnings ~= 0 and "  " .. buffer.diagnostics.warnings)
-              or ""
-          end,
-          fg = function(buffer)
-            return (buffer.diagnostics.errors ~= 0 and errors_fg)
-              or (buffer.diagnostics.warnings ~= 0 and warnings_fg)
-              or nil
-          end,
-          truncation = { priority = 1 },
-        },
-        close_or_unsaved = {
-          text = function(buffer)
-            return buffer.is_modified and "●" or ""
-          end,
-          fg = function(buffer)
-            return buffer.is_modified and green or nil
-          end,
-          delete_buffer_on_left_click = true,
-          truncation = { priority = 1 },
-        },
-        icon_indicator = {
-          text = function(buffer)
-            return buffer.is_focused and " 󰋱" or " "
-          end,
-          truncation = { priority = 1 },
-        },
-      }
-
-      require("cokeline").setup({
-        buffers = {
-          filter_valid = function(buffer)
-            return buffer.type ~= "terminal" and buffer.filetype ~= "neo-tree"
-          end,
-          filter_visible = function(buffer)
-            return buffer.type ~= "terminal" and buffer.filetype ~= "neo-tree"
-          end,
-          new_buffers_position = "next",
-        },
-        rendering = {
-          max_buffer_width = 30,
-        },
-        default_hl = {
-          fg = function(buffer)
-            return buffer.is_focused and get_hex("ColorColumn", "fg")
-          end,
-        },
-        components = {
-          components.separator,
-          components.space,
-          components.devicon,
-          components.space,
-          components.index,
-          components.space,
-          components.unique_prefix,
-          components.filename,
-          components.diagnostics,
-          components.two_spaces,
-          components.icon_indicator,
-          components.space,
-          components.close_or_unsaved,
-          components.space,
-          components.separator,
         },
       })
     end,
