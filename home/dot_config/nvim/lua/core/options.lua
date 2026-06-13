5acee883d95168f6d602bb4f07b351b4637809e1
Anifyuli <anifyuli007@outlook.co.id>
2026-06-13 18:53:49 +0700

    feat(nvim): tree-sitter based code folding with foldcolumn

--- home/dot_config/nvim/lua/core/options.lua
+++ home/dot_config/nvim/lua/core/options.lua
@@ -31,6 +31,14 @@ opt.wrap = true
 
 opt.fillchars:append({ eob = " " })
 
+-- Code folding with tree-sitter
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldcolumn = "1"
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldenable = true
+
 -- Auto adjust window size
 opt.equalalways = true
 
