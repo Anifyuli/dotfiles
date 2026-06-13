0cfa4f5b5045d91552d4837cb8c0a3b0054c95d9
Anifyuli <anifyuli007@outlook.co.id>
2026-06-13 21:37:22 +0700

    fix(nvim): nvim-ts-autotag modern opts format

--- home/dot_config/nvim/lua/plugins/coding.lua
+++ home/dot_config/nvim/lua/plugins/coding.lua
@@ -290,9 +290,11 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     event = "InsertEnter",
     opts = {
-      enable_close = true,
-      enable_rename = true,
-      enable_close_on_slash = true,
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
     },
     config = function(_, opts)
       require("nvim-ts-autotag").setup(opts)
