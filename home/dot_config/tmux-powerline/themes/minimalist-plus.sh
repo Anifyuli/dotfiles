10c11520ba3061bd3292d2fa9125cdb3ee0c3f45
Anifyuli <anifyuli007@outlook.co.id>
2026-06-14 17:03:25 +0700

    chore(tmux): truncate window basename to 10 chars for compact status line

--- home/dot_config/tmux-powerline/themes/minimalist-plus.sh
+++ home/dot_config/tmux-powerline/themes/minimalist-plus.sh
@@ -34,7 +34,7 @@ TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR="$FG"
 # Window status current (active window)
 TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
     "#[bg=${BG_WIN},fg=${FG}]"
-    " #I#F #{b:window_name} "
+    " #I#F #{b:window_name:10} "
     "#[bg=default,fg=${FG}]"
     " "
 )
@@ -47,7 +47,7 @@ TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
 # Window format
 TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
     "#[bg=${BG_WIN},fg=${FG}]"
-    " #I#{?window_flags,#F, } #{b:window_name} "
+    " #I#{?window_flags,#F, } #{b:window_name:10} "
 )
 
 # Left side: session, pwd, branch
